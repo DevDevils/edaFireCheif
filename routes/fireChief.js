3469ac9c113e212bfc771fb3900f3cc61d35bb24
zimsteve <zimsteve@live.com>
2016-11-04 12:15:00 +1300

    made changes to the index route and hbs template

--- routes/fireChief.js
+++ routes/fireChief.js
@@ -5,5 +5,5 @@ module.exports = router;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {title: 'edaFireChief'});
+  res.render('index', { title: 'Express' });
 });
