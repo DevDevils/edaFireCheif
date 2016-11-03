fe837f16495b26fdf53314bcea159030d598107c
zimsteve <zimsteve@live.com>
2016-11-04 12:34:54 +1300

    fireChief route exists bit not working yet

--- routes/fireChief.js
+++ routes/fireChief.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+var minions = require('../db/eda')
 
-module.exports = router;
+module.exports = router
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+/* GET fireChief page. */
+router.get('/', (req, res) => {
+  minions.isOnsite()
+    .then((minonsOnSite) => {
+      res.render('fireChief', { name: 'minonsOnSite' })
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+})
